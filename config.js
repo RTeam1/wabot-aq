d9ee53a41cc7ef07cd2206eff3ea408a46cfbb06
Dinda Zahra Yuliana <77674437+RTeam1@users.noreply.github.com>
2021-10-26 18:14:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6281515860089', '6281334177340'] // Put your number here
+global.owner = ['6289677763976', '6283808135990', '79224816516'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -15,7 +15,7 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://api.xteam.xyz': '9db5a5373433c015',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.xyz': 'apivinz',
   'https://pencarikode.xyz': 'pais',
@@ -27,8 +27,11 @@ global.APIKeys = { // APIKey Here
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'I hope you\'re fine'
-  var sticker_author = 'Nurutomo'
+ // var sticker_name = 'I hope you\'re fine'
+  //var sticker_author = 'Nurutomo'
+  var sticker_name = '饾檷饾櫄饾櫀饾櫋 饾檨饾櫀饾櫌饾櫒 饾檰饾櫈饾櫋饾櫋饾櫄饾櫑' // ganti aja
+
+var sticker_author = '' // ganti aja
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
