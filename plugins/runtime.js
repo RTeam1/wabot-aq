ae9a2527fac8c052edf1000d8b2bc93aad590e11
Dinda Zahra Yuliana <77674437+RTeam1@users.noreply.github.com>
2021-10-26 19:26:59 +0700

    Add files via upload

---
+++ plugins/runtime.js
@@ -0,0 +1,47 @@
+let handler  = async (m, { conn, Presence }) => {
+let { performance } = require('perf_hooks')
+let fs = require('fs')
+
+function kyun(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var hours = Math.floor(seconds / (60*60));
+  var minutes = Math.floor(seconds % (60*60) / 60);
+  var seconds = Math.floor(seconds % 60);
+
+  //return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
+  return `${pad(hours)}Jam ${pad(minutes)}Menit ${pad(seconds)}Detik`
+}
+					runtime = process.uptime()
+					teks = `𝙍𝙚𝙖𝙡 𝙁𝙖𝙢𝙨 𝙆𝙞𝙡𝙡𝙚𝙧\n\n${kyun(runtime)}`
+					var itsme = `0@s.whatsapp.net`
+					var split = `teks`
+					const rtimebro = {
+					contextInfo: {
+					participant: itsme,
+					quotedMessage: {
+					extendedTextMessage: {
+					text: split,
+									}
+								}
+							}
+					}
+					m.reply(teks)
+	}
+handler.help = ['runtime']
+handler.tags = ['info']
+handler.command = /^runtime$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 3
+
+module.exports = handler
