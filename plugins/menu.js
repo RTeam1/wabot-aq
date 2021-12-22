5cd78b76ea7ede1e4f982effb56b9292785f592b
RTeam1 <77674437+RTeam1@users.noreply.github.com>
2021-12-22 23:34:56 +0700

    Add files via upload

--- plugins/menu.js
+++ plugins/menu.js
@@ -167,7 +167,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
       totalexp: exp,
       xp4levelup: max - exp,
       github: package.homepage ? package.homepage.url || package.homepage : '[unknown github url]',
-      level, limit, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg, role,
+      level, limit, name, tagsender, weton, week, date, dateIslamic, time, totalreg, rtotalreg, role,
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
