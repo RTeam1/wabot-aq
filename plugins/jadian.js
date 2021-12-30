daa6cb0c1d7954b839aa3a19bfeeceb6fe248e9d
RTeam1 <77674437+RTeam1@users.noreply.github.com>
2021-12-30 08:42:45 +0700

    Update jadian.js

--- plugins/jadian.js
+++ plugins/jadian.js
@@ -1,22 +1,17 @@
-let R = Math.random
-let Fl = Math.floor
-let toM = a => '@' + a.split('@')[0]
-function handler(m, { groupMetadata }) {
-    let ps = groupMetadata.participants.map(v => v.jid)
-    let a = ps[Fl(R() * ps.length)]
-    let b
-    do b = ps[Fl(R() * ps.length)]
-    while (b === a)
-    m.reply(`${toM(a)} ❤️ ${toM(b)}`, null, {
-        contextInfo: {
-            mentionedJid: [a, b]
-        }
-    })
+let handler = async (m, { conn, participants, usedPrefix, command }) => {
+let member = participants.map(u => u.jid)
+let orang
+if (/ku/i.test(command)) orang = m.sender
+else orang = member[Math.floor(Math.random() * member.length)]
+let jodoh = member[Math.floor(Math.random() * member.length)]
+let jawab = `@${orang.replace(/@.+/, '')} ❤️ @${jodoh.replace(/@.+/, '')}`.trim()
+let mentionedJid = [orang, jodoh]
+//conn.reply(m.chat, jawab, m, { contextInfo: { mentionedJid } })
+await conn.sendButton(m.chat, jawab, footer, 'Jadian💘♥️❤️😘', `${usedPrefix + command}`, m, { contextInfo: { mentionedJid } })
 }
-handler.help = ['jadian']
-handler.tags = ['main']
-handler.command = ['jadian']
+handler.help = ['jodohin', 'jodohku']
+handler.tags = ['fun']
+handler.command = /^jodoh(in|ku)|jadian$/i
 handler.group = true
-handler.limit = true
 
 module.exports = handler
