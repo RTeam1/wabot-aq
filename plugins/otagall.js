97c3336b30025e0dec1781caed2664d249174c1d
RTeam1 <77674437+RTeam1@users.noreply.github.com>
2022-01-02 21:10:03 +0700

    Create otagall.js

---
+++ plugins/otagall.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn, text, participants }) => {
+  let users = participants.map(u => u.jid)
+  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
+    contextInfo: { mentionedJid: users }
+  })
+}
+handler.command = ['otagall']
+
+handler.owner = true
+handler.group = true
+
+module.exports = handler
